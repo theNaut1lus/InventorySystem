23045fa9a85dbd210ac6d5fe81b8f795d9cae340
Sidney Aulakh <sidneyaulakh@gmail.com>
2024-10-16 15:38:59 +1100

    Create connection static class and import packages

---
+++ DBConnection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventorySystem
+{
+    internal static class DBConnection
+    {
+        public static string Connection
+        {
+            get
+            {
+                //change the connection string to your own local SQL Server connection string
+                return "Data Source=sidney;Initial Catalog=C:\\USERS\\ADMINISTRATOR\\DOCUMENTS\\INVENTORYDB.MDF;Integrated Security=True;Encrypt=False;";
+            }
+        }
+    }
+}

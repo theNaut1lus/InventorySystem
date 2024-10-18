77a87bb327b1a609a414f3d307ca2e7a8c08a14f
YuLayKo <40755960+YuLayKo@users.noreply.github.com>
2024-10-18 12:52:20 +1100

    Update Category.cs
    for DB functions

--- Category.cs
+++ Category.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,31 +13,18 @@ namespace InventorySystem
 {
     public partial class Category : Form
     {
+        SqlConnection Con = null;
+        DBUtil dBUtil = new DBUtil();
         public Category()
         {
             InitializeComponent();
+            string connect = DBConnection.Connection;
+            Con = new SqlConnection(connect);
         }
-
-        SqlConnection Con = new SqlConnection("Data Source=YLK-HOME\\SQLEXPRESS;Database=C:\\Users\\Administrator\\Documents\\inventorydb.mdf;Integrated Security=True;Connect Timeout=30");
 
         void popularate()
         {
-            try
-            {
-                Con.Open();
-                string Myqurey = "select * from CatogariesTbl";
-                SqlDataAdapter da = new SqlDataAdapter(Myqurey, Con);
-                SqlCommandBuilder builder = new SqlCommandBuilder(da);
-                var ds = new DataSet();
-                da.Fill(ds);
-                DGCategory.DataSource = ds.Tables[0];
-                Con.Close();
-
-            }
-            catch
-            {
-
-            }
+            dBUtil.popularate(Con, DGCategory, "CatogariesTbl");
         }
 
         private void Category_Load(object sender, EventArgs e)
@@ -47,39 +34,17 @@ namespace InventorySystem
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
-            try
-            {
-                Con.Open();
-                SqlCommand cmd = new SqlCommand("insert into CatogariesTbl values('" + txtCatalogID.Text + "','" + txtCatalogName.Text + "')", Con);
-                cmd.ExecuteNonQuery();
-                MessageBox.Show("Category Successfully Added");
-                Con.Close();
-                popularate();
-            }
-            catch
-            {
+            dBUtil.insertDB(Con, "CatogariesTbl", "'" + txtCatalogID.Text + "', '" + 
+                txtCatalogName.Text + "'", "Category Successfully Added");
+            popularate();
 
-
-            }
         }
 
         private void btnEdit_Click(object sender, EventArgs e)
         {
-            try
-            {
-                Con.Open();
-                SqlCommand cmd = new SqlCommand("update CatogariesTbl set CatName='" +
-                    txtCatalogName.Text + "' where CatId='" + txtCatalogID.Text + "'", Con);
-                cmd.ExecuteNonQuery();
-                MessageBox.Show("Catogary Successfully Updated");
-                Con.Close();
-                popularate();
-            }
-            catch
-            {
-
-
-            }
+            dBUtil.updateDB(Con, "CatogariesTbl", "CatName='" +
+                    txtCatalogName.Text + "' where CatId='" + txtCatalogID.Text + "'", "Catogary Successfully Updated");
+            popularate();
         }
 
         private void DGCategory_CellContentClick(object sender, DataGridViewCellEventArgs e)
@@ -96,12 +61,7 @@ namespace InventorySystem
             }
             else
             {
-                Con.Open();
-                string Myquary = "delete from CatogariesTbl where CatId='" + txtCatalogID.Text + "'";
-                SqlCommand cmd = new SqlCommand(Myquary, Con);
-                cmd.ExecuteNonQuery();
-                MessageBox.Show("Catogary Successfully Deleted");
-                Con.Close();
+                dBUtil.deleteDB(Con, "CatogariesTbl", "CatId='" + txtCatalogID.Text + "'", "Catogary Successfully Deleted");
                 popularate();
             }
         }

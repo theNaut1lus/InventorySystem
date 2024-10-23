2e9b7adf7d13449cd5d028782e35eadef77c3a0f
Rifat Helal <rifathelal@outlook.com>
2024-10-24 03:33:21 +1100

    Added Customer screen

---
+++ Customer.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace InventorySystem
+{
+    partial class Customer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            gridCustomer = new DataGridView();
+            tbCusName = new TextBox();
+            tbCusPhone = new TextBox();
+            groupBox1 = new GroupBox();
+            tbCusID = new TextBox();
+            label3 = new Label();
+            btnClear = new Button();
+            btnUpsert = new Button();
+            label2 = new Label();
+            label1 = new Label();
+            tbSearch = new TextBox();
+            btnSearchClear = new Button();
+            btnSearch = new Button();
+            ((System.ComponentModel.ISupportInitialize)gridCustomer).BeginInit();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // gridCustomer
+            // 
+            gridCustomer.AllowUserToAddRows = false;
+            gridCustomer.AllowUserToDeleteRows = false;
+            gridCustomer.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            gridCustomer.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            gridCustomer.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            gridCustomer.Location = new Point(181, 0);
+            gridCustomer.Name = "gridCustomer";
+            gridCustomer.ReadOnly = true;
+            gridCustomer.Size = new Size(467, 309);
+            gridCustomer.TabIndex = 0;
+            gridCustomer.CellClick += SelectCustomer;
+            // 
+            // tbCusName
+            // 
+            tbCusName.Location = new Point(6, 81);
+            tbCusName.Name = "tbCusName";
+            tbCusName.PlaceholderText = "John Doe";
+            tbCusName.Size = new Size(149, 23);
+            tbCusName.TabIndex = 1;
+            // 
+            // tbCusPhone
+            // 
+            tbCusPhone.Location = new Point(6, 125);
+            tbCusPhone.Name = "tbCusPhone";
+            tbCusPhone.PlaceholderText = "0XXXXXXXXX";
+            tbCusPhone.Size = new Size(149, 23);
+            tbCusPhone.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(tbCusID);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(btnClear);
+            groupBox1.Controls.Add(btnUpsert);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(tbCusPhone);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(tbCusName);
+            groupBox1.Location = new Point(12, 88);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(163, 218);
+            groupBox1.TabIndex = 3;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Customer";
+            // 
+            // tbCusID
+            // 
+            tbCusID.Location = new Point(6, 37);
+            tbCusID.Name = "tbCusID";
+            tbCusID.PlaceholderText = "1001";
+            tbCusID.Size = new Size(149, 23);
+            tbCusID.TabIndex = 8;
+            tbCusID.TextChanged += SetCustomer;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(7, 19);
+            label3.Name = "label3";
+            label3.Size = new Size(18, 15);
+            label3.TabIndex = 6;
+            label3.Text = "ID";
+            // 
+            // btnClear
+            // 
+            btnClear.Location = new Point(6, 183);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(149, 23);
+            btnClear.TabIndex = 5;
+            btnClear.Text = "Clear";
+            btnClear.UseVisualStyleBackColor = true;
+            btnClear.Click += ClearCustomer;
+            // 
+            // btnUpsert
+            // 
+            btnUpsert.Location = new Point(6, 154);
+            btnUpsert.Name = "btnUpsert";
+            btnUpsert.Size = new Size(149, 23);
+            btnUpsert.TabIndex = 4;
+            btnUpsert.Text = "Insert";
+            btnUpsert.UseVisualStyleBackColor = true;
+            btnUpsert.Click += UpsertCustomer;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(6, 107);
+            label2.Name = "label2";
+            label2.Size = new Size(88, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Phone Number";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(6, 63);
+            label1.Name = "label1";
+            label1.Size = new Size(39, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Name";
+            // 
+            // tbSearch
+            // 
+            tbSearch.Location = new Point(18, 12);
+            tbSearch.Name = "tbSearch";
+            tbSearch.PlaceholderText = "Search...";
+            tbSearch.Size = new Size(123, 23);
+            tbSearch.TabIndex = 4;
+            // 
+            // btnSearchClear
+            // 
+            btnSearchClear.Location = new Point(147, 12);
+            btnSearchClear.Name = "btnSearchClear";
+            btnSearchClear.Size = new Size(23, 23);
+            btnSearchClear.TabIndex = 5;
+            btnSearchClear.Text = "X";
+            btnSearchClear.UseVisualStyleBackColor = true;
+            btnSearchClear.Click += ClearSearch;
+            // 
+            // btnSearch
+            // 
+            btnSearch.Location = new Point(18, 41);
+            btnSearch.Name = "btnSearch";
+            btnSearch.Size = new Size(152, 23);
+            btnSearch.TabIndex = 6;
+            btnSearch.Text = "Search";
+            btnSearch.UseVisualStyleBackColor = true;
+            btnSearch.Click += Search;
+            // 
+            // Customer
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(648, 309);
+            Controls.Add(btnSearch);
+            Controls.Add(btnSearchClear);
+            Controls.Add(tbSearch);
+            Controls.Add(groupBox1);
+            Controls.Add(gridCustomer);
+            MinimumSize = new Size(664, 348);
+            Name = "Customer";
+            Text = "Customer";
+            Load += LoadCustomers;
+            ((System.ComponentModel.ISupportInitialize)gridCustomer).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView gridCustomer;
+        private TextBox tbCusName;
+        private TextBox tbCusPhone;
+        private GroupBox groupBox1;
+        private Label label2;
+        private Label label1;
+        private Button btnUpsert;
+        private TextBox tbSearch;
+        private Button btnSearchClear;
+        private Button btnClear;
+        private Button btnSearch;
+        private Label label3;
+        private TextBox tbCusID;
+    }
+}
---
+++ Customer.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventorySystem
+{
+    public partial class Customer : Form
+    {
+        SqlConnection? dbCon = null;
+        DBUtil dBUtil = new();
+
+        public Customer()
+        {
+            InitializeComponent();
+            string connect = DBConnection.Connection;
+            dbCon = new SqlConnection(connect);
+        }
+
+        private void PopulateData()
+        {
+            dBUtil.popularate(dbCon, gridCustomer, "CustomerTbl");
+        }
+
+        private void LoadCustomers(object sender, EventArgs e)
+        {
+            PopulateData();
+        }
+
+        private void UpsertCustomer(object sender, EventArgs e)
+        {
+            if (tbCusID.Text == "")
+            {
+                dBUtil.insertDB(dbCon, "CustomerTbl", $"{tbCusID.Text}, '{tbCusName.Text}', '{tbCusPhone.Text}'", "Customer inserted successfully!");
+            } else
+            {
+                dBUtil.updateDB(dbCon, "CustomerTbl", $"CusName='{tbCusName.Text}', CusPhone='{tbCusPhone.Text}' where CusID='{tbCusID.Text}'", "Customer updated successfully!");
+            }
+
+            PopulateData();
+        }
+
+        private void SelectCustomer(object sender, DataGridViewCellEventArgs e)
+        {
+            Console.WriteLine(gridCustomer.Rows[gridCustomer.CurrentCell.RowIndex].Cells.ToString());
+            var row = gridCustomer.Rows[gridCustomer.CurrentCell.RowIndex].Cells;
+            tbCusID.Text = row[0].Value.ToString();
+            tbCusID.ReadOnly = true;
+            tbCusName.Text = row[1].Value.ToString();
+            tbCusPhone.Text = row[2].Value.ToString();
+        }
+
+        private void Search(object sender, EventArgs e)
+        {
+            dBUtil.searchDB(gridCustomer, tbSearch.Text);
+        }
+
+        private void ClearSearch(object sender, EventArgs e)
+        {
+            tbSearch.Text = "";
+            PopulateData();
+        }
+
+        private void SetCustomer(object sender, EventArgs e)
+        {
+            btnUpsert.Text = tbCusID.Text == "" ? "Insert" : "Update";
+        }
+
+        private void ClearCustomer(object sender, EventArgs e)
+        {
+            tbCusID.Text = "";
+            tbCusID.ReadOnly = false;
+            tbCusName.Text = "";
+            tbCusPhone.Text = "";
+        }
+    }
+}
